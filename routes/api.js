5098d1b4a40de031858966a2e709b1664f7dd71d
PickHD <taufikjanuar35@gmail.com>
2021-01-30 13:56:05 +0700

    Add api routes

---
+++ routes/api.js
@@ -0,0 +1,28 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert')
+    .get(function (req, res){
+      let input = req.query.input;
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      res.send(toString);
+    });
+    
+};
